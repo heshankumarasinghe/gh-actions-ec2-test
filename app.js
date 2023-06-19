a92356c1aa478e9b923b80ecf74db98468b00145
Heshan Kumarasinghe <heshan.kumarasinghe@outlook.com>
2023-06-19 10:47:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/v1/users', (req, res) => {
+    return res.status(200).json({
+        status: 'success',
+        message: 'Hello, World!',
+        data: null
+    });
+});
+
+app.listen(8080, '0.0.0.0', () => {
+    console.log('App is listening on PORT 8080')
+});
